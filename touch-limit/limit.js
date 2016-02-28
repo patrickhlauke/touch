30957aafd72bd359d5b9f63af688f197cb3ba0d9
Patrick H. Lauke <redux@splintered.co.uk>
2016-02-28 00:57:05 +0000

    Update to latest limit.js

--- touch-limit/limit.js
+++ touch-limit/limit.js
@@ -1,18 +1,19 @@
 /**
  * debounce
  * @param {integer} milliseconds This param indicates the number of milliseconds
- *     to wait after the last call before calling the original function .
+ *     to wait after the last call before calling the original function.
+ * @param {object} What "this" refers to in the returned function.
  * @return {function} This returns a function that when called will wait the
  *     indicated number of milliseconds after the last call before
  *     calling the original function.
  */
-Function.prototype.debounce = function (milliseconds) {
+Function.prototype.debounce = function (milliseconds, context) {
     var baseFunction = this,
         timer = null,
         wait = milliseconds;
 
     return function () {
-        var self = this,
+        var self = context || this,
             args = arguments;
 
         function complete() {
@@ -32,17 +33,18 @@ Function.prototype.debounce = function (milliseconds) {
 * throttle
 * @param {integer} milliseconds This param indicates the number of milliseconds
 *     to wait between calls before calling the original function.
+* @param {object} What "this" refers to in the returned function.
 * @return {function} This returns a function that when called will wait the
 *     indicated number of milliseconds between calls before
 *     calling the original function.
 */
-Function.prototype.throttle = function (milliseconds) {
+Function.prototype.throttle = function (milliseconds, context) {
     var baseFunction = this,
         lastEventTimestamp = null,
         limit = milliseconds;
 
     return function () {
-        var self = this,
+        var self = context || this,
             args = arguments,
             now = Date.now();
 
